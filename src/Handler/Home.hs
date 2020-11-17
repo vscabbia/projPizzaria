aa86dbd6a9e1846d08dd654a203ca27356a03fce
Vitor Scabbia <vcscabbia@hotmail.com>
2020-11-17 00:46:37 -0300

    Home page

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -15,18 +15,33 @@ getHomeR = defaultLayout $ do
   addStylesheet (StaticR css_bootstrap_css)
   [whamlet|
 
-        <h1>
-            SISTEMA DE SABOR
-        
-        <img src=@{StaticR img_sabor_jpg}>
-        
-        <ul>
-            <li> 
-                <a href=@{SaborR}>
-                    CADASTRO DE SABOR
-            <li> 
-                <a href=@{CardapioR}>
-                    CARDAPIO
+        <h1 class="ml-3">
+            Pizzaria
+        <div class="container-fluid" style="background-image: url(@{StaticR img_sabor_jpg});height:33em;background-size:cover">
+            
+            
+                <div class="container">
+                        <div class="row pt-3">
+                            <div class="d-flex justify-content-center">
+
+                                <a class="mx-2" class="btn btn-light" href=@{SaborR}>
+                                    Cadastrar sabor
+                            
+                                <a class="btn btn-light mx-2" href=@{CardapioR}>
+                                    Ver cardápio
+                            
+                                <a class="btn btn-light mx-2" href=@{CrPedidoR}>
+                                    Fazer pedido
+
+                                <a class="btn btn-light mx-2" href=@{UnidadeR}>
+                                    Cadastrar unidades
+                            
+                                <a class="btn btn-light mx-2" href=@{ListUnidadeR}>
+                                    Ver unidades
+                            
+                        
+
+
     |]
 
 getCardapioR :: Handler Html
@@ -37,8 +52,28 @@ getCardapioR = do
     [whamlet|
             <h1>Cardápio
             
-            <ul>
-                $forall Entity sid sabor <- sabors
-                    <li> 
-                        #{saborNome sabor}
+            <table class=" table table-striped ">
+                    <thead>
+                        <tr>
+                            <th> 
+                                Sabor
+                            
+                            <th>
+                                Preço
+                            
+                            <th>
+                                Descrição
+                            <th>
+                    <tbody>
+                    $forall Entity _ sabor <- sabors   
+                        <tr>
+                                <td>
+                                    #{saborNome sabor}
+                                
+                                <td>
+                                    #{saborPreco sabor}
+                                
+                                <th>
+                                    #{saborDescricao sabor}
+                                        
         |]
